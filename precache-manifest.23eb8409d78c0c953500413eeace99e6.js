980995be9f0ad99e248e2090b5663a315605349a
Disquietude <danny93ngo@gmail.com>
2018-11-10 04:45:05 -0500

    Updates

---
+++ precache-manifest.23eb8409d78c0c953500413eeace99e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef1a603158ce56d01ba",
+    "url": "/Markdown-Previewer/static/js/runtime~main.1ef1a603.js"
+  },
+  {
+    "revision": "c786ab38fe62ba0d4b4a",
+    "url": "/Markdown-Previewer/static/js/main.c786ab38.chunk.js"
+  },
+  {
+    "revision": "be4869aeecce430ad0d9",
+    "url": "/Markdown-Previewer/static/js/1.be4869ae.chunk.js"
+  },
+  {
+    "revision": "c786ab38fe62ba0d4b4a",
+    "url": "/Markdown-Previewer/static/css/main.62580d97.chunk.css"
+  },
+  {
+    "revision": "dfed56935d69808d65c352751510e2bb",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
